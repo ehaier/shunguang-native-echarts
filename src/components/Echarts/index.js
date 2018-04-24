13ff83f173a7cc99d5c593880ea53e5f29d0b456
Phoenix Z Sun <phoenixzsun@outlook.com>
2018-04-24 17:48:21 +0800

    add all files

---
+++ src/components/Echarts/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { WebView, View, StyleSheet, Platform } from 'react-native';
+import renderChart from './renderChart';
+import echarts from './echarts.min';
+
+const source = (Platform.OS == 'ios') ? require('./tpl.html') : {'uri':'file:///android_asset/echarts/tpl.html'}
+
+export default class App extends Component {
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.option !== this.props.option) {
+      this.refs.chart.reload();
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, height: this.props.height || 400,}}>
+        <WebView
+          ref="chart"
+          scrollEnabled = {false}
+          injectedJavaScript = {renderChart(this.props)}
+          scalesPageToFit={Platform.OS === 'android' ? true : false}
+          style={{
+            height: this.props.height || 400,
+          }}
+          source={source}
+          style={{backgroundColor: this.props.option.backgroundColor || 'rgba(0, 0, 0, 0)'}}
+          onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
+        />
+      </View>
+    );
+  }
+}
