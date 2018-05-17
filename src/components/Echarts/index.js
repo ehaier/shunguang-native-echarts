465c82901c0d77c9051c3cadb4df44b21c11e601
Phoenix Z Sun <phoenixzsun@outlook.com>
2018-05-17 09:46:32 +0800

    修复ios下图表闪烁的问题

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -6,12 +6,25 @@ import echarts from './echarts.min';
 const source = (Platform.OS == 'ios') ? require('./tpl.html') : {'uri':'file:///android_asset/echarts/tpl.html'}
 
 export default class App extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    const thisProps = this.props || {};
+    nextProps = nextProps || {};
+    if (Object.keys(thisProps).length !== Object.keys(nextProps).length) {
+        return true;
+    }
+    for (const key in nextProps) {
+        if (JSON.stringify(thisProps[key]) != JSON.stringify(nextProps[key])) {
+            return true;
+        }
+    }
+    return false;
+  }
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
-      this.refs.chart.reload();
+    if (nextProps.option !== this.props.option) {
+// 解决数据改变时页面闪烁的问题
+      this.refs.chart.injectJavaScript(renderChart(nextProps, true));
     }
   }
-
   render() {
     return (
       <View style={{flex: 1, height: this.props.height || 400,}}>
